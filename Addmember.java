c27f20e070ced5266a2cfec87293ea644fa1fe6d
vani <vanibaluputta@gmail.com>
2024-12-02 22:57:23 +0530

    firstcommit

---
+++ Addmember.java
@@ -0,0 +1,20 @@
+package test;
+
+import sailpoint.object.Identity;
+ import sailpoint.object.ProvisioningPlan;
+ import sailpoint.object.ProvisioningPlan.AccountRequest;
+ import sailpoint.object.ProvisioningPlan.AttributeRequest;
+ import sailpoint.object.Application;
+ import sailpoint.api.Provisioner;
+ import javax.naming.directory.ModificationItem;
+ import javax.naming.directory.BasicAttribute;
+ import javax.naming.directory.Attribute;
+ import javax.naming.Context;
+ import javax.naming.directory.DirContext;
+ import javax.naming.NamingException;
+ import java.util.Hashtable;
+public class Addmember {
+	
+	
+
+}
